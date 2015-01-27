28cdc79b2e3432ec6f1c2cb3c83f17453ca60efe
Paul Hammant <paul@hammant.org>
2015-01-27 05:40:03 -0500

    check in diff data

--- calculate_differences_between_tests.sh
+++ calculate_differences_between_tests.sh
@@ -2,30 +2,26 @@
 
 mkdir coverage_diff_data
 
-declare -A processed
-
 for test1 in `find coverage_data -name "*#*" | sed 's#coverage_data/##' | sed 's/\.txt//'`; do
     for test2 in `find coverage_data -name "*#*" | sed 's#coverage_data/##' | sed 's/\.txt//'`; do
-        if [[ "$test1" != "$test2" ]]; then
-            if [[ "$test1" > "$test2" ]]; then
-                one=$test2
-                two=$test1
-            else
-                one=$test1
-                two=$test2
-            fi
-            output="coverage_diff_data/${one}-${two}.diff"
-            if [[ ${processed["$one $two"]} == "" ]];then
-                diff "coverage_data/${one}.txt" "coverage_data/${two}.txt" >> "${output}"
-                deleted=$(grep -c "^<" "${output}")
-                added=$(grep -c "^>" "${output}")
-                matching=$(fgrep -xcf "coverage_data/${one}.txt" "coverage_data/${two}.txt")
-                if [[ ( "$added" == "0" || "$deleted" == "0" ) && "$matching" != "0" ]]; then
-                    echo "$one and $two are very very close"
+        if [[ "$test1" < "$test2" ]]; then
+            output="coverage_diff_data/${test1}-${test2}.diff"
+            diff "coverage_data/${test1}.txt" "coverage_data/${test2}.txt" >> "${output}"
+            deleted=$(grep -c "^<" "${output}")
+            added=$(grep -c "^>" "${output}")
+            matching=$(fgrep -xcf "coverage_data/${test1}.txt" "coverage_data/${test2}.txt")
+            if [[ "$matching" != "0" ]]; then
+                if [[ ( "$added" == "0" && "$deleted" == "0" ) ]]; then
+                    echo "$test1 and $test2 are covering exactly the same lines"
+                elif [[ ( "$added" == "0" && "$deleted" != "0" ) ]]; then
+                    echo "$test2 covers the same lines as $test1 (and more)"
+                elif [[ ( "$added" != "0" && "$deleted" == "0" ) ]]; then
+                    echo "$test1 covers the same lines as $test2 (and more)"
                 else
                     rm "${output}"
                 fi
-                processed["$one $two"]=1
+            else
+                rm "${output}"
             fi
         fi
     done
